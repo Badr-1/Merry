2cfe956def09ea47d94592007a3c8a66fc9dd530
Badr-1 <badrmohamedashraf@gmail.com>
2024-06-20 16:33:57 +0300

    added animations

--- app/src/main/java/com/example/military/MainActivity.kt
+++ app/src/main/java/com/example/military/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.military
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +20,14 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.StrokeCap
@@ -28,6 +40,8 @@ import androidx.compose.ui.unit.sp
 import com.example.military.ui.theme.Brown
 import com.example.military.ui.theme.DarkGreen
 import com.example.military.ui.theme.LightGreen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.util.Locale
 
@@ -41,11 +55,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+const val ANIMATION_DURATION = 2000
+
 @Composable
-fun YearProgress(progress: Float, modifier: Modifier = Modifier) {
+fun YearProgress(targetProgress: Float, modifier: Modifier = Modifier) {
+    val animatedProgress by animateFloatAsState(
+        targetValue = targetProgress,
+        animationSpec = tween(durationMillis = ANIMATION_DURATION)
+    )
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
         CircularProgressIndicator(
-            progress = { progress },
+            progress = { animatedProgress },
             modifier = Modifier
                 .size(300.dp)
                 .background(LightGreen, CircleShape),
@@ -56,7 +76,7 @@ fun YearProgress(progress: Float, modifier: Modifier = Modifier) {
 
             )
         Text(
-            text = String.format(Locale.getDefault(), "%.2f", progress * 100) + "%",
+            text = String.format(Locale.getDefault(), "%.2f", animatedProgress * 100) + "%",
             fontSize = 64.sp,
             fontFamily = FontFamily.Monospace,
             fontWeight = FontWeight.Bold,
@@ -69,12 +89,43 @@ fun YearProgress(progress: Float, modifier: Modifier = Modifier) {
 @Preview
 @Composable
 fun PreviewYearProgress() {
-    YearProgress(progress = 1.0f)
+    YearProgress(targetProgress = 1.0f)
 }
+
+const val TAG = "MainActivity"
 
 @Composable
 @Preview(showBackground = true)
 fun MilitaryScreen(modifier: Modifier = Modifier) {
+    var progress by remember { mutableFloatStateOf(0f) }
+    var passedDaysProgress by remember { mutableIntStateOf(0) }
+    var servedDaysProgress by remember { mutableIntStateOf(0) }
+    var leftDaysProgress by remember { mutableIntStateOf(0) }
+    val coroutineScope = rememberCoroutineScope()
+    val targetProgress = calculateProgress()
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            while (progress < targetProgress) {
+                progress += 0.0001f
+            }
+        }
+        coroutineScope.launch {
+            while (passedDaysProgress < getPassedDays()) {
+                passedDaysProgress += 1
+            }
+        }
+        coroutineScope.launch {
+            while (servedDaysProgress < getServedDays()) {
+                servedDaysProgress += 1
+            }
+        }
+        coroutineScope.launch {
+            while (leftDaysProgress < getLeftDays()) {
+                leftDaysProgress += 1
+            }
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -83,19 +134,19 @@ fun MilitaryScreen(modifier: Modifier = Modifier) {
     ) {
         Spacer(modifier = Modifier.height(64.dp))
         Text(
-            "Welcome, Solider",
+            stringResource(R.string.welcome_solider),
             color = LightGreen,
             fontWeight = FontWeight.Normal,
             fontSize = 32.sp,
             fontFamily = FontFamily.Monospace
         )
         Spacer(modifier = Modifier.height(32.dp))
-        YearProgress(progress = calculateProgress())
+        YearProgress(targetProgress = progress)
         Spacer(modifier = Modifier.weight(1f))
         Details(
-            leftDays = stringResource(R.string.remaining_days, getLeftDays()),
-            passedDays = stringResource(R.string.passed_days, getPassedDays()),
-            servedDays = stringResource(R.string.served_days, getServedDays())
+            leftDays = leftDaysProgress,
+            passedDays = passedDaysProgress,
+            servedDays = servedDaysProgress
         )
         Spacer(modifier = Modifier.weight(1f))
     }
@@ -103,15 +154,26 @@ fun MilitaryScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun Details(
-    leftDays: String,
-    passedDays: String,
-    servedDays: String,
+    leftDays: Int,
+    passedDays: Int,
+    servedDays: Int,
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
-
+        val leftDaysAnimated by animateIntAsState(
+            targetValue = leftDays,
+            animationSpec = tween(durationMillis = ANIMATION_DURATION)
+        )
+        val passedDaysAnimated by animateIntAsState(
+            targetValue = passedDays,
+            animationSpec = tween(durationMillis = ANIMATION_DURATION)
+        )
+        val servedDaysAnimated by animateIntAsState(
+            targetValue = servedDays,
+            animationSpec = tween(durationMillis = ANIMATION_DURATION)
+        )
         Text(
-            passedDays,
+            stringResource(id = R.string.passed_days, passedDaysAnimated.toString().padStart(3,'0')),
             color = LightGreen,
             fontWeight = FontWeight.Normal,
             fontSize = 32.sp,
@@ -119,7 +181,7 @@ fun Details(
             modifier = Modifier.padding(8.dp)
         )
         Text(
-            servedDays,
+            stringResource(R.string.served_days, servedDaysAnimated.toString().padStart(3,'0')),
             color = LightGreen,
             fontWeight = FontWeight.Normal,
             fontSize = 32.sp,
@@ -128,7 +190,7 @@ fun Details(
 
         )
         Text(
-            leftDays,
+            stringResource(R.string.left_days, leftDaysAnimated.toString().padStart(3,'0')),
             color = LightGreen,
             fontWeight = FontWeight.Normal,
             fontSize = 32.sp,
